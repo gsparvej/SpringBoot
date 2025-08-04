19b5e09f27f1d65308efa80be4d510a1ecd1818a
gsparvej <parvejmd4469@gmail.com>
2025-08-04 12:44:57 +0600

    added

--- demoProject/demoProject/src/main/java/com/gsparvej/demoProject/security/SecurityConfig.java
+++ demoProject/demoProject/src/main/java/com/gsparvej/demoProject/security/SecurityConfig.java
@@ -4,6 +4,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
-@EnableWebSecurity
+@EnableWebSecurity 
 public class SecurityConfig {
 }
